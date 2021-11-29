b0d60d52adeccce82fd35ffb1e93fb23940097a0
Ishit Beswal <ib370@snu.edu.in>
2021-11-29 05:33:49 +0530

    feature: file complaint form static

---
+++ components/Select.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { faSortDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import styles from '../styles/Select.module.css';
+
+const Select = ({
+    placeholder,
+    values,
+    defaultValue,
+    classNames,
+    ...extra
+}) => {
+    let [optionsOpen, setOptionsOpen] = useState(false);
+    let [selectedValue, setSelectedValue] = useState(defaultValue);
+
+    return (
+        <div
+            className={`${styles.select} ${classNames}`}
+            {...extra}
+            onMouseEnter={() => setOptionsOpen(true)}
+            onMouseLeave={() => setOptionsOpen(false)}
+        >
+            {selectedValue ? selectedValue.text : placeholder}
+            <div
+                className={`${styles.options} ${
+                    optionsOpen && styles.show
+                } border-radius-10 background-black-translucent glass-effect w-75`}
+                style={{
+                    '--values': optionsOpen ? values.length : 0,
+                }}
+            >
+                {values.map((value, index) => (
+                    <div
+                        className={`${styles.option}`}
+                        key={value.value}
+                        style={{
+                            transform: !optionsOpen
+                                ? `translateY(-${index * 100}%)`
+                                : 'translateY(0)',
+                        }}
+                        onClick={() => setSelectedValue(value)}
+                    >
+                        {value.text}
+                    </div>
+                ))}
+            </div>
+            <FontAwesomeIcon icon={faSortDown} className={styles.selectIcon} />
+        </div>
+    );
+};
+
+export default Select;
--- pages/citizen/file-complaint.jsx
+++ pages/citizen/file-complaint.jsx
@@ -1,10 +1,116 @@
+import { useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSortDown } from '@fortawesome/free-solid-svg-icons';
+import CustomBackground from '../../components/CustomBackground';
+
+import styles from '../../styles/FileComplaint.module.css';
+import Select from '../../components/Select';
+
 const FileComplaint = () => {
+    let select = useRef();
     return (
-        <div styles='file-complaint'>
-            <h1 className='heading-text-foreground-white'>
-                File a new Complaint
-            </h1>
-        </div>
+        <CustomBackground>
+            <div
+                className={`${styles.fileComplaint} background-black-translucent glass-effect border-radius-15`}
+            >
+                <h1
+                    className={`heading-text bold-text uppercase-text center-text foreground-white`}
+                    style={{
+                        fontSize: '3.75rem',
+                    }}
+                >
+                    Lodge Complaint
+                </h1>
+                <h1 className='subheading-text center-text foreground-primary'>
+                    Please enter the details as accurately as you can
+                </h1>
+                <form className={`${styles.form}`} action=''>
+                    <input
+                        required
+                        className={`${styles.inputText}`}
+                        type='text'
+                        placeholder='Enter Place of Incident'
+                    />
+                    <input
+                        required
+                        className={`${styles.inputText}`}
+                        type='text'
+                        placeholder='Enter Pin Code'
+                    />
+                    <input
+                        required
+                        className={`${styles.inputText}`}
+                        type='text'
+                        placeholder='Select Nearest Police Station'
+                    />
+                    {/* <input
+                        required
+                        className={`${styles.inputText}`}
+                        style={{
+                            color: 'var(--clr-white)',
+                        }}
+                        type='datetime-local'
+                        placeholder='Select Date and Time of Incident'
+                    /> */}
+                    {/* <input
+                        required
+                        className={`${styles.inputText}`}
+                        type='text'
+                        placeholder='Choose type of Offense'
+                    /> */}
+                    <div className={styles.selectContainer}>
+                        <select
+                            ref={select}
+                            defaultValue='0'
+                            name='offense'
+                            id='offense-select'
+                            className={`${styles.inputText} ${styles.select} foreground-white`}
+                        >
+                            <option value='0'>
+                                Please choose offense type
+                            </option>
+                            <option value='rape'>Rape</option>
+                            <option value='harass'>Harass</option>
+                            <option value='dowry'>Dowry</option>
+                            <option value='theft'>Theft</option>
+                        </select>
+                        <FontAwesomeIcon
+                            icon={faSortDown}
+                            className={styles.selectIcon}
+                        />
+                    </div>
+                    <Select
+                        placeholder='Choose type of Offense'
+                        values={[
+                            { text: 'Rape', value: 'rape' },
+                            { text: 'Harass', value: 'harass' },
+                            { text: 'Dowry', value: 'dowry' },
+                            { text: 'Theft', value: 'theft' },
+                        ]}
+                        classNames={`foreground-white ${styles.inputText}`}
+                    />
+                    <textarea
+                        required
+                        // rows={10}
+                        // cols={40}
+                        className={`${styles.inputText} ${styles.textArea}`}
+                        placeholder='Describe the Incident as accurately as possible'
+                    ></textarea>
+                    <textarea
+                        required
+                        // rows={10}
+                        // cols={40}
+                        className={`${styles.inputText} ${styles.textArea}`}
+                        placeholder='Give a description of the suspect as best as you can'
+                    ></textarea>
+                    <button
+                        className={`submit-button dark border-radius-10 w-100 ${styles.btn}`}
+                    >
+                        LODGE COMPLAINT
+                    </button>
+                </form>
+            </div>
+        </CustomBackground>
     );
 };
 
