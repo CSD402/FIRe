af02c24739ea5da10a28481589a7ec87faef317f
Ishit Beswal <ib370@snu.edu.in>
2021-12-06 01:58:04 +0530

    added timestamps to fir

--- src/fir/schemas/fir.schema.ts
+++ src/fir/schemas/fir.schema.ts
@@ -1,32 +1,37 @@
 import * as mongoose from 'mongoose';
 var Schema = mongoose.Schema;
 
-export const FirSchema = new mongoose.Schema({
-  complaint_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'Complaint',
-    required: true,
-    unique: true,
-    immutable: true,
+export const FirSchema = new mongoose.Schema(
+  {
+    complaint_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'Complaint',
+      required: true,
+      unique: true,
+      immutable: true,
+    },
+    approved_by: {
+      type: Schema.Types.ObjectId,
+      ref: 'PoliceOfficer',
+      required: true,
+      immutable: true,
+    },
+    comments: {
+      type: String,
+      immutable: true,
+    },
+    officer_name: {
+      type: String,
+      required: true,
+      immutable: true,
+    },
+    officer_phone: {
+      type: Number,
+      immutable: true,
+      required: true,
+    },
   },
-  approved_by: {
-    type: Schema.Types.ObjectId,
-    ref: 'PoliceOfficer',
-    required: true,
-    immutable: true,
+  {
+    timestamps: true,
   },
-  comments: {
-    type: String,
-    immutable: true,
-  },
-  officer_name: {
-    type: String,
-    required: true,
-    immutable: true,
-  },
-  officer_phone: {
-    type: Number,
-    immutable: true,
-    required: true,
-  },
-});
+);
